883c16fb8735fcb8fb81504e862fa00b18fed3c9
ilaydacrt26 <ilayda.cirit26@gmail.com>
2025-08-12 15:06:23 +0300

    bugfix

--- utils/rag_system.py
+++ utils/rag_system.py
@@ -1,9 +1,6 @@
 import os
 import sys
 
-__import__('pysqlite3')
-sys.modules['sqlite3'] = sys.modules['pysqlite3']
-
 import chromadb
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import GooglePalmEmbeddings
